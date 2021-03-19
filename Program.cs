2ea6cf8673e94b52bbfbe86021f63e990c0855b4
Алексей Валерьевич Белоусов <chiisai@yandex.ru>
2021-03-19 23:20:26 +0300

    5.2.3

--- Program.cs
+++ Program.cs
@@ -4,6 +4,42 @@ namespace SF5._2
 {
     class Program
     {
+        static string ShowColor(string username)
+        {
+            Console.WriteLine("{0}, напиши свой любимый цвет на английском с маленькой буквы:", username);
+            string color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    return color;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    return color;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    return color;
+                default:
+                    color = "yellow";
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    return color;
+            }
+        }
+
         static void Main(string[] args)
         {
             var (name, age) = ("Евгения", 276);
@@ -23,6 +59,19 @@ namespace SF5._2
             Console.WriteLine("Новое имя: {0}", name);
             Console.WriteLine("Возраст: {0}", age);
 
+            var favcolors = new string[3];
+
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor(name);
+            }
+
+            Console.WriteLine("Любимые цвета:");
+
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
         }
     }
 }
